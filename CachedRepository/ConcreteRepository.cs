449b739166f8cfb3b5438ada76a384daf2f791c6
Denis Nikolaevich Birulya <birulya.dn@ramecvs.ru>
2019-11-27 15:01:17 +0300

    up

--- CachedRepository/ConcreteRepository.cs
+++ CachedRepository/ConcreteRepository.cs
@@ -25,54 +25,57 @@ namespace CachedRepository
             return returnValue;
         }
 
-        public Task<object> GetCashedTask(Func<object[], object> getMetoDelegate,
-            object[] parameter, [CallerMemberName] string repositoryMethodName = null)
-        {
-            object returnValue = null;
 
-            if (TryGetFromCach(new[] { parameter }, out returnValue, repositoryMethodName))
+        public async Task<object> GetCashedTask(Task<object> getTask, object[] parameter,
+            [CallerMemberName] string repositoryMethodName = null)
+        {
+            if (TryGetFromCach(new object[] { parameter}, out var returnValue, repositoryMethodName))
             {
-                return returnValue;
+                return Task.FromResult(returnValue);
             }
 
-            returnValue = getMetoDelegate.Invoke(parameter);
-
-            SetCach(new[] { parameter }, returnValue, repositoryMethodName);
-            return returnValue;
-        }
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public IEnumerable<Model> Get( string parameter)
-        {
-            return GetCashed(arg =>
+            return getTask.ContinueWith(v =>
             {
-                #region Получение данных из БД
-
-                if (string.IsNullOrEmpty(parameter))
-                    return new List<Model>();
-
-                var newItem = new List<Model>();
-                int maxCount = 10;
-                int i = 0;
-                while (i < maxCount)
-                {
-                    i++;
-                    newItem.Add(new Model() { Id = Guid.NewGuid() });
-                }
-
-                #endregion
-                return newItem;
-            }, new object[] {parameter}) as IEnumerable<Model>;
+                SetCach(new object[] {parameter}, returnValue, repositoryMethodName);
+            });
 
         }
 
 
+        ///// <summary>
+        ///// </summary>
+        ///// <returns></returns>
+        //public IEnumerable<Model> Get( string parameter)
+        //{
+        //    return GetCashed(arg =>
+        //    {
+        //        #region Получение данных из БД
+        //        if (string.IsNullOrEmpty(parameter))
+        //            return new List<Model>();
+        //        var newItem = new List<Model>();
+        //        int maxCount = 10;
+        //        int i = 0;
+        //        while (i < maxCount)
+        //        {
+        //            i++;
+        //            newItem.Add(new Model() { Id = Guid.NewGuid() });
+        //        }
+        //        #endregion
+        //        return newItem;
+        //    }, new object[] {parameter}) as IEnumerable<Model>;
+        //}
 
 
+        public Task<Model> GetAsincCashed(string parameter)
+        {
+            var x= GetCashedTask(Get =>
+            {
+                return Task.FromResult(new Model());
+
+            }, new object[] {parameter});
+
+            return null;
+        }
 
         /// <summary>
         /// 
@@ -80,26 +83,10 @@ namespace CachedRepository
         /// <returns></returns>
         public Task<Model> GetAsinc(string parameter)
         {
-            return GetCashed(arg =>
-            {
-                #region Получение данных из БД
+            TaskCompletionSource<>
 
-                if (string.IsNullOrEmpty(parameter))
-                    return new List<Model>();
-
-                var newItem = new List<Model>();
-                int maxCount = 10;
-                int i = 0;
-                while (i < maxCount)
-                {
-                    i++;
-                    newItem.Add(new Model() { Id = Guid.NewGuid() });
-                }
-
-                #endregion
-                return newItem;
-            }, new object[] { parameter }) as IEnumerable<Model>;
-
+            Task<Model> task = new Task<Model>();
+               return task;
         }
     }
 }
